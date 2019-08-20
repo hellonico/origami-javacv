60300ebd912968fd1a8aa5269988ff0bb74ddfba
Nicolas Modrzyk <hellonico@gmail.com>
2019-08-20 17:29:10 +0900

    learning

--- src/smooth.clj
+++ src/smooth.clj
@@ -4,12 +4,17 @@
     (:import [org.bytedeco.opencv.opencv_core Size])
     (:import [org.bytedeco.opencv.global opencv_imgcodecs opencv_imgproc]))
 
-; (import [org.bytedeco.opencv.global opencv_core  opencv_imgcodecs])
 (defn -main[& args]
 (let[marcel (opencv_imgcodecs/imread "marcel.jpg")
-     canvas (CanvasFrame. "Marcel")
+     canvas (CanvasFrame. "Marcel" 0 nil)
      converter (OpenCVFrameConverter$ToMat.)]
-    (opencv_imgproc/GaussianBlur marcel marcel (Size. 3 3) 0.0)
+    (opencv_imgproc/GaussianBlur marcel marcel (Size. 5 5) 0.0)
     (opencv_imgproc/cvtColor marcel marcel opencv_imgproc/COLOR_BGR2GRAY)
     (.showImage canvas (.convert converter marcel))
     (opencv_imgcodecs/imwrite "output.jpg" marcel)))
+
+; ALT-F4 to close
+(comment
+(-main)
+
+)
--- src/webcam.clj
+++ src/webcam.clj
@@ -1,13 +1,19 @@
-(ns webcam)
-(import [org.bytedeco.javacv CanvasFrame])
-(import [org.bytedeco.javacv FrameGrabber])
+(ns webcam
+(:import [org.bytedeco.javacv CanvasFrame])
+(:import [org.bytedeco.javacv FrameGrabber]))
 
 (defn -main[& args]
     (let [ grabber (FrameGrabber/createDefault 0)
-           canvas (CanvasFrame. "Webcam")]
+        ;    canvas (CanvasFrame. "Webcam")
+        canvas (CanvasFrame. "Marcel" 0 nil)
+           ]
     (.start grabber)
     (while (.isVisible canvas)
        (.showImage canvas (.grab grabber)))
     (.close grabber)))
 
-(-main)
+
+(comment
+    (-main)
+    
+    )

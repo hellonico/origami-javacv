fbd3b7b32569daec8452c07fd8c1ce3d030b8d92
Nicolas Modrzyk <hellonico@gmail.com>
2019-08-21 16:43:49 +0900

    clojure notebook sample

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject javacvj "0.1.0-SNAPSHOT"
   :java-source-paths ["java"]
-  :main hello
-  :plugins [[lein-auto "0.1.3"]]
+  :main HelloCv
+  :plugins [[lein-auto "0.1.3"][lein-jupyter "0.1.16"]]
   :auto {:default {:file-pattern #"\.(java)$"}}
   :repositories [["vendredi" "https://repository.hellonico.info/repository/hellonico/"]]
   :dependencies [[org.clojure/clojure "1.10.0"]
